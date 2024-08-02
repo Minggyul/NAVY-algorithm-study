35233578d114e3cfb622c10f206b5d9e665767ad
luke <142500104+hongik-luke@users.noreply.github.com>
2024-08-02 16:51:47 +0900

    Add files via upload

---
+++ WEEK10/Gihyuen/2357.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+
+#define FastIO cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+
+using namespace std;
+int N,M;
+int arr[100010];
+void input(){
+    cin>>N>>M;
+    for(int i = 0 ; i < N ; i++){
+        cin>>arr[i];
+    }
+}
+int min_segtree[400000];
+int max_segtree[400000];
+void make_min_segtree(int lo ,int hi , int idx){
+    if(lo == hi){
+        min_segtree[idx] = arr[lo];
+        return;
+    }
+    int mid = (lo+hi)/2;
+    make_min_segtree(lo,mid,idx*2);
+    make_min_segtree(mid+1,hi,idx*2+1);
+
+    min_segtree[idx] = min(min_segtree[idx*2],min_segtree[idx*2+1]);
+    return ;
+}
+void make_max_segtree(int lo ,int hi , int idx){
+    if(lo == hi){
+        max_segtree[idx] = arr[lo];
+        return;
+    }
+    int mid = (lo+hi)/2;
+    make_max_segtree(lo,mid,idx*2);
+    make_max_segtree(mid+1,hi,idx*2+1);
+
+    max_segtree[idx] = max(max_segtree[idx*2], max_segtree[idx*2+1]);
+    return ;
+}
+int get_min(int lo , int hi ,int l ,int r , int idx){
+    if(l <= lo && hi <= r){
+        return min_segtree[idx];
+    }
+    else if(hi < l || r < lo){
+        return 2000000000;
+    }
+    int mid = (lo+hi)/2;
+    return min(get_min(lo,mid,l,r,idx*2), get_min(mid+1,hi,l,r,idx*2+1));
+    
+}
+
+int get_max(int lo , int hi ,int l ,int r , int idx){
+    if(l <= lo && hi <= r){
+        return max_segtree[idx];
+    }
+    else if(hi < l || r < lo){
+        return -1;
+    }
+    int mid = (lo+hi)/2;
+    return max(get_max(lo,mid,l,r,idx*2), get_max(mid+1,hi,l,r,idx*2+1));
+    
+}
+void print(){
+    int n1,n2;
+    for(int i = 1 ; i <= M ; i++){
+        cin>>n1>>n2;
+        n1--;
+        n2--;
+        cout<<get_min(0,N-1,n1,n2,1)<<" "<<get_max(0,N-1,n1,n2,1)<<"\n";
+    }
+}
+int main(){
+    FastIO;
+    input();
+    make_min_segtree(0, N-1, 1);
+    make_max_segtree(0, N-1, 1);
+    print();
+}
